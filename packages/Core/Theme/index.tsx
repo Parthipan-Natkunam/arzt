75a818e580e3b46c219a5c846afd38738ccabdfc
Parthipan Natkunam <natkunamparthipan@gmail.com>
2022-05-29 05:13:26 +0200

    add css resets and extend API to provide custom fonts

--- packages/Core/Theme/index.tsx
+++ packages/Core/Theme/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode } from "react";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { Theme } from "@arzt/core";
 
 export const ArztDefaultTheme: Theme = {
@@ -64,9 +64,99 @@ export const ArztDefaultTheme: Theme = {
 export type ArztThemeProviderProps = {
   children: ReactNode;
   theme?: Theme;
+  fontCSSURL?: string;
+  fontNames?: string[];
 };
+
+const CSSReset = createGlobalStyle<{ fontURL: string; fontNames: string[] }>`
+/* Custom Fonts */
+@import url('${(props) => props.fontURL}');
+
+/* Box sizing rules */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* Remove default margin */
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+figure,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+
+/* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+ul[role='list'],
+ol[role='list'] {
+  list-style: none;
+}
+
+/* Set core root defaults */
+html:focus-within {
+  scroll-behavior: smooth;
+}
+
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  text-rendering: optimizeSpeed;
+  line-height: 1.5;
+  font-family: ${(props) => props.fontNames.join(", ")};
+}
+
+/* A elements that don't have a class get default styles */
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+
+/* Make images easier to work with */
+img,
+picture {
+  max-width: 100%;
+  display: block;
+}
+
+/* Inherit fonts for inputs and buttons */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+@media (prefers-reduced-motion: reduce) {
+  html:focus-within {
+   scroll-behavior: auto;
+  }
+  
+  *,
+  *::before,
+  *::after {
+    animation-duration: 0.01ms !important;
+    animation-iteration-count: 1 !important;
+    transition-duration: 0.01ms !important;
+    scroll-behavior: auto !important;
+  }
+}
+`;
 
 export const ArztThemeProvider: FC<ArztThemeProviderProps> = ({
   children,
   theme = ArztDefaultTheme,
-}) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  fontCSSURL = "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
+  fontNames = ["Roboto", "sans-serif"],
+}) => (
+  <ThemeProvider theme={theme}>
+    <CSSReset fontURL={fontCSSURL} fontNames={fontNames} />
+    {children}
+  </ThemeProvider>
+);
